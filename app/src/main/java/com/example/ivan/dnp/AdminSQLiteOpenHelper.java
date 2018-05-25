4dc4c2d0ac572d5f66500a90e13f31f16296f326
isivanh <isivanh@gmail.com>
2018-05-25 11:39:20 -0500

    conexion entre activitys del main al sevice

---
+++ app/src/main/java/com/example/ivan/dnp/AdminSQLiteOpenHelper.java
@@ -0,0 +1,34 @@
+package com.example.ivan.dnp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+
+    public AdminSQLiteOpenHelper(Context context, String nombre, SQLiteDatabase.CursorFactory factory, int version) {
+
+        super(context, nombre, factory, version);
+
+    }
+
+    @Override
+
+    public void onCreate(SQLiteDatabase db) {
+
+        //aquí creamos la tabla de usuario (dni, nombre, ciudad, numero)
+        db.execSQL("create table user(dni integer primary key, nombre text, ciudad text, numero integer)");
+
+    }
+
+    @Override
+
+    public void onUpgrade(SQLiteDatabase db, int version1, int version2) {
+
+        db.execSQL("drop table if exists user");
+
+        db.execSQL("create table usuario(dni integer primary key, nombre text, ciudad text, numero integer)");
+
+    }
+}
--- app/src/main/java/com/example/ivan/dnp/ServiceActivity.java
+++ app/src/main/java/com/example/ivan/dnp/ServiceActivity.java
@@ -14,6 +14,6 @@ public class ServiceActivity extends AppCompatActivity {
         setContentView(R.layout.activity_service);
 
 
-    
+
     }
 }
