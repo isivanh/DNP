8d6d6869d09479ac748914bc86cf008fdb09814d
isivanh <isivanh@gmail.com>
2018-05-25 11:11:49 -0500

    conexion entre activitys del main al sevice

--- app/src/main/java/com/example/ivan/dnp/ServiceActivity.java
+++ app/src/main/java/com/example/ivan/dnp/ServiceActivity.java
@@ -1,7 +1,10 @@
 package com.example.ivan.dnp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ServiceActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class ServiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+
+    
     }
 }
